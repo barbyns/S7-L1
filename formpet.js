b7890af9e41b98af226b9c2760d0f4de55d78144
barbyns <barbyns95@gmail.com>
2025-03-17 17:02:48 +0100

    form

--- formpet.js
+++ formpet.js
@@ -3,12 +3,24 @@ const ownerNameInput = document.getElementById("ownerName")
 const speciesInput = document.getElementById("species")
 const breedInput = document.getElementById("breed")
 
-class PetName {
+class Pet {
     constructor(_PetName, _ownerName, _species, _breed) {
-this.PetName = _PetName
-this._ownerName = _ownerName
-this._species = _species
-this._breed = _breed 
+this.PetName = _PetName;
+this._ownerName = _ownerName;
+this._species = _species;
+this._breed = _breed ;
     }
 }
-const form = document.getElementById("user-form")
+const form = document.getElementById("user-form");
+
+form.addEventListener("submit", function(event){
+event.preventDefault();
+console.log("INVIO DEL FORM");
+const newPet = new Pet(
+    PetNameInput.value.trim(),
+    ownerNameInput.value.trim(),
+    speciesInput.value.trim(),
+    breedInput.value.trim()
+);
+console.log("Nuovo per registrato:", newPet);
+});
